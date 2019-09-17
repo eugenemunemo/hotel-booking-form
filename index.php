4a525109ff8d4fb49eee321e5d5f22e296b9bee5
eugenemunemo <j.babybehindmusic@gmail.com>
2019-09-17 14:53:04 -0700

    Update index.php
    add rev of yr dream hotel prices

--- index.php
+++ index.php
@@ -44,3 +44,18 @@ session_start();
           <a class="dropdown-item" href="#">Wine</a>
         </div>
       </li>
+
+      <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<h1>Reservation for your  dream Hotel</h1>
+<div class="price">
+<p id="inn">Holiday Inn: R 200</p>
+<p id="rad">Radison: R 100</p>
+<p id="city">City Lodge: R 400</p>
+<p id="town">Town Lodge: R 150</p>
+</div>
